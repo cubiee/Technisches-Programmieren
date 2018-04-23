a45e03e132db2267c42b62d6d76a364f0a200324
jharig <j.harig95@hotmail.de>
2018-04-23 21:57:36 +0200

    uebung 8 verschluesselung funktioniert fürs erste

--- Uebung8/main.c
+++ Uebung8/main.c
@@ -3,28 +3,55 @@
 #include <string.h>
 #define BUFFERSIZE 10000
 
+void menue(void);
+
 int main(int argc, char *argv[]){
     if(argc < 3){
         menue();
     }else{
+
         FILE *klartext;
         FILE *verschluesselt;
+
         if(NULL == (klartext = fopen(argv[1], "r"))){
             printf("Konnte datei nicht öffnen\n");
-        }else if(NULL == (verschluesselt = fopen("verschluesselt.txt", "r"))){
+        }else if(NULL == (verschluesselt = fopen("verschluesselt.txt", "w"))){
             printf("Konnte datei nicht öffnen\n");
         }else{
             char dateipuffer[BUFFERSIZE] = { 0 };
-            char zeilenpuffer[BUFFERSIZE] = { 0 };
+            char zeilenpuffer[1000] = { 0 };
+
             int startpoint = 0;
-            while(NULL == (fgets(zeilenpuffer, BUFFERSIZE, klartext))){
-                for(int i = startpoint; i < (startpoint + strlen(zeilenpuffer); i++)){
+			int i = 0;
+            while((NULL != (fgets(zeilenpuffer, 1000, klartext))) && (startpoint < BUFFERSIZE - 1000)){
+
+                for(i = startpoint; i < (startpoint + strlen(zeilenpuffer)); i++){
                     dateipuffer[i] = zeilenpuffer[i - startpoint]; 
                 }
                 startpoint += strlen(zeilenpuffer);
             }
-            printf("%s", )
+			for (i = 0; i < strlen(dateipuffer); i++){
+				if ((dateipuffer[i] >= 'A') && (dateipuffer[i] < 'Z')){
+					dateipuffer[i] += 1 ;
+				}
+				else if (dateipuffer[i] == 'Z'){
+					dateipuffer[i] = 'A';
+				}
+				else if ((dateipuffer[i] >= 'a') && (dateipuffer[i] < 'z')){
+					dateipuffer[i] += 1;
+				}
+				else if (dateipuffer[i] == 'z'){
+					dateipuffer[i] = 'a';
+				}
+			}
+			fputs(dateipuffer, verschluesselt);
         }
     }
     return 0;
+}
+
+
+void menue(void){
+	printf("Aufruf durch:\n");
+	printf("\t./verschlüsselung <dateiname> <versatz>\n");
 }
