65a1b9b11107a85abb148f2c4c79ef90407568b7
jharig <j.harig95@hotmail.de>
2018-04-27 13:45:04 +0200

    Uebung 8 verbessert

--- Uebung8/main.c
+++ Uebung8/main.c
@@ -1,51 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define BUFFERSIZE 10000
+#define TRUE 1
+#define FALSE 0
 
 void menue(void);
+int load_file(char *filename, char **buffer);
+int write_file(char *filename, char *data);
+
+char verschluesseln(char character, int n);
 
 int main(int argc, char *argv[]){
-    if(argc < 3){
+    if(argc < 4){
         menue();
     }else{
-
-        FILE *klartext;
-        FILE *verschluesselt;
-
-        if(NULL == (klartext = fopen(argv[1], "r"))){
-            printf("Konnte datei nicht öffnen\n");
-        }else if(NULL == (verschluesselt = fopen("verschluesselt.txt", "w"))){
-            printf("Konnte datei nicht öffnen\n");
-        }else{
-            char dateipuffer[BUFFERSIZE] = { 0 };
-            char zeilenpuffer[1000] = { 0 };
-
-            int startpoint = 0;
-			int i = 0;
-            while((NULL != (fgets(zeilenpuffer, 1000, klartext))) && (startpoint < BUFFERSIZE - 1000)){
-
-                for(i = startpoint; i < (startpoint + strlen(zeilenpuffer)); i++){
-                    dateipuffer[i] = zeilenpuffer[i - startpoint]; 
-                }
-                startpoint += strlen(zeilenpuffer);
-            }
-			for (i = 0; i < strlen(dateipuffer); i++){
-				if ((dateipuffer[i] >= 'A') && (dateipuffer[i] < 'Z')){
-					dateipuffer[i] += 1 ;
-				}
-				else if (dateipuffer[i] == 'Z'){
-					dateipuffer[i] = 'A';
-				}
-				else if ((dateipuffer[i] >= 'a') && (dateipuffer[i] < 'z')){
-					dateipuffer[i] += 1;
-				}
-				else if (dateipuffer[i] == 'z'){
-					dateipuffer[i] = 'a';
-				}
+		char *datei_inhalt;
+		int characters_loaded = 0;
+		if ((characters_loaded = load_file(argv[2], &datei_inhalt)) < 0){
+			printf("Eingabedatei konnte nicht geöffnet werden!\n");
+			menue();
+		}
+		else{
+			for (int i = 0; i < characters_loaded; i++){
+				datei_inhalt[i] = verschluesseln(datei_inhalt[i], atoi(argv[1]));
 			}
-			fputs(dateipuffer, verschluesselt);
-        }
+			write_file(argv[3], datei_inhalt);
+		}
     }
     return 0;
 }
@@ -53,5 +33,52 @@ int main(int argc, char *argv[]){
 
 void menue(void){
 	printf("Aufruf durch:\n");
-	printf("\t./verschlüsselung <dateiname> <versatz>\n");
+	printf("\t./geheim <schluessel> <eingabedatei> <ausgabedatei>\n");
+}
+
+int load_file(char *filename, char **buffer){
+	FILE *datei;
+	if (NULL == (datei = fopen(filename, "r"))){
+		return -1;
+	}
+	else
+	{
+		fseek(datei, 0, SEEK_END);
+		long anzahl_zeichen = ftell(datei);
+		*buffer = malloc((anzahl_zeichen + 1)*sizeof(char));
+		rewind(datei);
+		for (int i = 0; i < anzahl_zeichen; i++){
+			(*buffer)[i] = fgetc(datei);
+		}
+		fclose(datei);
+		(*buffer)[anzahl_zeichen] = '\0';
+		return anzahl_zeichen;
+	}
+}
+
+int write_file(char *filename, char *data){
+	FILE *datei;
+	if (NULL == (datei = fopen(filename, "w"))){
+		return FALSE;
+	}
+	else
+	{
+		fputs(data, datei);
+		fclose(datei);
+		return TRUE;
+	}
+}
+
+char verschluesseln(char character, int key){
+	key = key % 26;
+	if (character >= 'a' && character <= 'z'){
+		return ((character - 'a' + key) % 26) + 'a';
+	}
+	if (character >= 'A' && character <= 'Z'){
+		return ((character - 'A' + key) % 26) + 'A';
+	}
+	else
+	{
+		return character;
+	}
 }
